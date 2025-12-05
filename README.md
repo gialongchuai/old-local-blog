2d5530d2072dff933225b0580a8b4ebc308de231
gialongchuai <gialongchuai@gmail.com>
2025-12-05 15:48:43 +0700

    begin nhớ đọc readme dùm log

---
+++ README.md
@@ -0,0 +1,10 @@
+```
+yarn create react-app old-local-blog --template typescript
+cd old-local-blog
+
+yarn add @reduxjs/toolkit@1.8.6 react-redux@8.0.4
+
+yarn add -D tailwindcss@3.1.8 postcss@8.4.18 autoprefixer@10.4.12
+
+yarn tailwindcss init -p
+```
---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import CreatePost from './page/blog/component/CreatePost/CreatePost';
+import Blog from './page/blog/component';
+
+function App() {
+  return (
+    <>
+    <Blog />
+    </>
+  );
+}
+
+export default App;
